5bf57a3cc75f4f79cd88cba4a3d80236d2efce10
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-03-21 18:02:33 -0300

    adiciona requisito 2 e 3

---
+++ script.js
@@ -0,0 +1,28 @@
+// cria o núcle que é uma section com id color-palette
+const father = document.getElementsByTagName('body')[0];
+const son = document.createElement('section');
+father.appendChild(son);
+const sectionFather = document.createElement('section');
+son.id = 'color-palette';
+father.appendChild(sectionFather);
+
+// cria os elementos
+function creatElements(element, classes) {
+  for (let i = 1; i <= 4; i += 1) {
+    const father1 = document.querySelector('section');
+    const son4 = document.createElement(element);
+    father1.appendChild(son4);
+    son4.classList = classes;
+  }
+}
+creatElements('div', 'color');
+
+// cria a paleta de cores
+function creatPallet() {
+  for (let i = 1; i <= 25; i += 1) {
+    const son25 = document.createElement('section');
+    son25.classList = 'stylePallet';
+    sectionFather.appendChild(son25);
+  }
+}
+creatPallet();
